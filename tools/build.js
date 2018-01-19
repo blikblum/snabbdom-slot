450c09ef09906f545f410f4cfa8b36800ec931f0
Luiz Américo <luizamericop@gmail.com>
2018-01-19 08:45:55 -0300

    Change exported name to snabbdomSlot

--- tools/build.js
+++ tools/build.js
@@ -25,7 +25,7 @@ promise = promise.then(() => del(['dist/*']));
     file: `dist/${format === 'umd' ? 'index' : 'index.esm'}.js`,
     format,
     sourcemap: true,
-    name: format === 'umd' ? pkg.name : undefined
+    name: format === 'umd' ? 'snabbdomSlot' : undefined
   })))
 })
 
