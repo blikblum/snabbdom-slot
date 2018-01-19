1497255a60364c0cd8ae3a2ced95c376e156db90
Luiz Américo <luizamericop@gmail.com>
2018-01-18 23:15:16 -0300

    Add more tests

--- test/withslot.spec.js
+++ test/withslot.spec.js
@@ -4,52 +4,92 @@ import { withSlot } from '../src/withslot'
 import render from './render-tree'
 import Snabbdom from 'snabbdom-pragma'
 
-describe('withSlot', () => {
-  describe('when the component has no slot', () => {
-    const NoSlot = withSlot(() => {
-      return (
-        <div>
-          <div>No slot</div>
-        </div>
-      )
-    })
-    it('children is ignored', () => {
+describe('when the component has no slot', () => {
+  const NoSlot = withSlot(() => {
+    return (
+      <div>
+        <div>No slot</div>
+      </div>
+    )
+  })
+  it('children is ignored', () => {
+    const tree = (
+      <NoSlot>
+        <div>Child element</div>
+      </NoSlot>
+    )
+    expect(render(tree)).toMatchSnapshot()
+  })
+})
+
+describe('when the component has a main slot', () => {
+  const MainSlot = withSlot(() => {
+    return (
+      <div>
+        <slot>Default content</slot>
+      </div>
+    )
+  })
+
+  describe('and no children is passed', () => {
+    it('should show default content', () => {
       const tree = (
-        <NoSlot>
-          <div>Child element</div>
-        </NoSlot>
+        <MainSlot/>
       )
       expect(render(tree)).toMatchSnapshot()
     })
   })
 
-  describe('when the component has a main slot', () => {
-    const MainSlot = withSlot(() => {
-      return (
-        <div>
-          <slot>Default content</slot>
-        </div>
+  describe('and children is passed', () => {
+    it('should show children content', () => {
+      const tree = (
+        <MainSlot>
+          <h1>Child element</h1>
+          <div>Other child element</div>
+        </MainSlot>
       )
+      expect(render(tree)).toMatchSnapshot()
     })
+  })
+})
 
-    describe('and no children is passed', () => {
-      it('should show default content', () => {
-        const tree = (
-          <MainSlot/>
-        )
-        expect(render(tree)).toMatchSnapshot()
-      })
+describe('when the component has a named slot', () => {
+  const NamedSlot = withSlot(() => {
+    return (
+      <div>
+        <slot name='myslot'>Default Content</slot>
+      </div>
+    )
+  })
+
+  describe('and no children is passed', () => {
+    it('should show default content', () => {
+      const tree = (
+        <NamedSlot/>
+      )
+      expect(render(tree)).toMatchSnapshot()
     })
+  })
 
-    describe('and children is passed', () => {
-      it('should show children content', () => {
-        const tree = (
-          <MainSlot>
-            <h1>Child element</h1>
-          </MainSlot>
-        )
-        expect(render(tree)).toMatchSnapshot()
-      })
+  describe('and children without matching slot prop is passed', () => {
+    it('should show default content', () => {
+      const tree = (
+        <NamedSlot>
+          <h1 slot='myotherslot'>Child element</h1>
+        </NamedSlot>
+      )
+      expect(render(tree)).toMatchSnapshot()
+    })
+  })
+
+  describe('and a child with matching slot prop is passed', () => {
+    it('should show child element in correspondent slot element', () => {
+      const tree = (
+        <NamedSlot>
+          <h1 slot='myslot'>Child element</h1>
+        </NamedSlot>
+      )
+      expect(render(tree)).toMatchSnapshot()
     })
   })
 })
